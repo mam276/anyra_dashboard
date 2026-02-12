5de6abf54db2b28e6713bb3d463b37682e849ac6
mam276 <148658679+mam276@users.noreply.github.com>
2026-02-13 00:28:59 +0530

    Update branding.py

--- src/utils/branding.py
+++ src/utils/branding.py
@@ -1,7 +1,8 @@
 import streamlit as st
 
 def show_branding():
-    st.sidebar.image("assets/anyra_logo.png", use_column_width=True)
+    st.sidebar.image("assets/anyra_logo.png", width=None)
     st.sidebar.markdown("### Anyra Dashboard")
     st.sidebar.markdown("**Author:** ASHRAF")
+
     st.sidebar.markdown("---")
