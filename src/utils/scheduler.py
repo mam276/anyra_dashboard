c009a3f9668b69df20ced86d4479cc448735160f
mam276 <148658679+mam276@users.noreply.github.com>
2026-02-12 23:23:39 +0530

    Update scheduler.py

--- src/utils/scheduler.py
+++ src/utils/scheduler.py
@@ -2,7 +2,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from utils.subscription import check_trials
 from utils.file_manager import delete_expired_files
 
-scheduler = BackgroundScheduler()
+scheduler = None
 
 def daily_report_generation():
     pass  # placeholder
@@ -11,8 +11,13 @@ def scheduled_cleanup(retention_days=30):
     delete_expired_files(retention_days=retention_days)
 
 def start_scheduler():
-    scheduler.add_job(check_trials, "interval", hours=24)
-    scheduler.add_job(daily_report_generation, "interval", hours=24)
-    scheduler.add_job(scheduled_cleanup, "interval", hours=24)
-    scheduler.start()
+    global scheduler
+    if scheduler is None:
+        scheduler = BackgroundScheduler()
+        scheduler.start()
+        scheduler.add_job(check_trials, "interval", hours=24)
+        scheduler.add_job(daily_report_generation, "interval", hours=24)
+        scheduler.add_job(scheduled_cleanup, "interval", hours=24)
+        scheduler.start()
+
 
